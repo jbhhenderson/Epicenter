6dedccc18e1916b48a40a38d70e85433c09f7583
Jackson Henderson <jbhhenderson@gmail.com>
2023-03-17 16:27:56 -0500

    just need to add login/register styling

--- epicenter/src/games/MyFavorites.js
+++ epicenter/src/games/MyFavorites.js
@@ -7,7 +7,8 @@ import { getUsersFavoritedGames } from "../providers/ApiManager"
 export const MyFavorites = () => {
     const [games, setGames] = useState([])
     const [libraryGames, setLibraryGames] = useState([])
-    
+    const [screenHeight, setScreenHeight] = useState("")
+
     const localEpicenterUser = localStorage.getItem("epicenter_user")
     const epicenterUserObject = JSON.parse(localEpicenterUser)
 
@@ -27,9 +28,16 @@ export const MyFavorites = () => {
 
             Promise.all(newArray)
             .then((gameArrays) => {
-                setGames(gameArrays.reduce((arr, e) => {
-                    return arr.concat(e)
-                }))
+                if (gameArrays.length) {
+                    setGames(gameArrays.reduce((arr, e) => {
+                        return arr.concat(e)
+                    }))
+                }
+                if (gameArrays.length >  4) {
+                    setScreenHeight("full")
+                } else {
+                    setScreenHeight("screen")
+                }
             })
         })  
     }
@@ -50,30 +58,43 @@ export const MyFavorites = () => {
         })
     }
 
-    return <>
-    <h1>My Favorites</h1>
-    <Link to={'/addGame'} state={{
-            games: games,
-            user: epicenterUserObject
-        }}>Add a Game</Link>
-    <ul>
+    return <div>
         {
-            games.map(game => {
-                return <li key={game.id}>
-                    <div>
-                        <Link to={`/gameInfo/${game.id}`}><p>{game.name}</p></Link>
+            games.length ?
+            
+        <div className={`bg-gray-300 pt-24 h-${screenHeight}`}>
+            <div className="grid gap-12 grid-cols-4 pl-24">
+                {
+                    games.map(game => {
+                        return <div
+                        className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700" key={game.id}>
+                        <a href={`http://localhost:3000/gameInfo/${game.id}`}>
+                            {
+                                game.cover?.url ? <img src={game.cover.url.replace("thumb", "720p")} loading="lazy" className="rounded-t-lg" alt=""/>
+                                :<img className="rounded-t-lg" loading="lazy" alt="" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fno-image-available-sign-vector-id1138179183%3Fk%3D6%26m%3D1138179183%26s%3D612x612%26w%3D0%26h%3DprMYPP9mLRNpTp3XIykjeJJ8oCZRhb2iez6vKs8a8eE%3D&f=1&nofb=1&ipt=7727c264fde6fb0879a2b7b4ff990b45ce06dc0dd368db646c6be4f8356018b7&ipo=images" />
+                            }
+                        </a>
+                        <div className="p-6">
+                        <h5
+                            className="mb-2 pb-1 text-xl font-medium leading-tight text-center text-neutral-800 dark:text-neutral-50 border-b-black border-b-2">
+                            {game.name}
+                        </h5>
+                        <div>
+                            {
+                                isFavorite(game) ? <button className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow shadow-green-500 transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-red-300 focus:bg-primary-600 focus:shadow-green-300 focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-green-600"
+                                data-te-ripple-init
+                                data-te-ripple-color="light"
+                                onClick={(clickEvent) => handleRemoveFavorite(clickEvent, game)}>Remove from favorites</button>
+                                : ""
+                            }
+                        </div>
+                        </div>
                     </div>
-                    {
-                        game.cover?.url ? <img src={game.cover.url} />
-                        :<img className="w-28" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fno-image-available-sign-vector-id1138179183%3Fk%3D6%26m%3D1138179183%26s%3D612x612%26w%3D0%26h%3DprMYPP9mLRNpTp3XIykjeJJ8oCZRhb2iez6vKs8a8eE%3D&f=1&nofb=1&ipt=7727c264fde6fb0879a2b7b4ff990b45ce06dc0dd368db646c6be4f8356018b7&ipo=images" />
-                    }
-                    {
-                        isFavorite(game) ? <button onClick={(clickEvent) => handleRemoveFavorite(clickEvent, game)}>Remove from favorites</button>
-                        : ""
-                    }                 
-                </li>
-            })
+                    })
+                }
+            </div>
+        </div>
+        : <div className={`bg-gray-300 pt-72px h-screen`}></div>
         }
-    </ul>
-    </>
+    </div>
 }
--- epicenter/src/games/TopGames.js
+++ epicenter/src/games/TopGames.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom"
 export const TopGames = () => {
 
     const [games, setGames] = useState([])
-    const [topTenGames, setTopTenGames] = useState([])    
+    const [topTenGames, setTopTenGames] = useState([]) 
+    const [screenHeight, setScreenHeight] = useState("")
+   
     useEffect(() => {
         getFavoritedGames()
         .then((gameArray) => {
@@ -21,6 +23,11 @@ export const TopGames = () => {
                 return acc
             }, [])
             setGames(reducedGames)
+            if (gameArray.length >  3) {
+                setScreenHeight("full")
+            } else {
+                setScreenHeight("screen")
+            }
         })
     }, [])
 
@@ -51,25 +58,52 @@ export const TopGames = () => {
 
     }, [games])
 
-    return <>
-    Top {topTenGames.length} Games Favorited by Users
-   
+    return <div>
+        {
+            topTenGames.length ?
+
+    <div className={`pt-20 bg-gray-300 h-full`}>
+    <div className="text-4xl pl-72 pt-6">Top {topTenGames.length} Games Favorited by Users:</div>
+   <ul>
     {
-        topTenGames.map((game) => {return<>
-            <li key={game.id}>
-            <Link to={`/gameInfo/${game.id}`}><p>{game.name}</p></Link> Favorites: {game.favoriteAmount}
-                {
-                    game.cover?.url ? <img src={game.cover.url} />
-                    :<img className="w-28" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fno-image-available-sign-vector-id1138179183%3Fk%3D6%26m%3D1138179183%26s%3D612x612%26w%3D0%26h%3DprMYPP9mLRNpTp3XIykjeJJ8oCZRhb2iez6vKs8a8eE%3D&f=1&nofb=1&ipt=7727c264fde6fb0879a2b7b4ff990b45ce06dc0dd368db646c6be4f8356018b7&ipo=images" />
-                }
-                {
-                    game.summary ? game.summary
-                    : ""
-                }
+        topTenGames.map((game) => {return<li key={game.id}>
+            <div className="mx-auto px-60 py-12">
+                <div className="flex dark:bg-neutral-700 border border-gray-300 rounded-xl overflow-hidden justify-start">
+                    <div className="relative w-60 h-60 flex-shrink-0">
+                        <div className="left-0 top-0 w-full h-full flex items-center justify-center">
+                        <Link to={`/gameInfo/${game.id}`}>
+                            {
+                                game.cover?.url ? <img alt="Placeholder Photo" loading="lazy" className="absolute left-0 top-0 w-full h-full object-cover object-center transition duration-50" src={`${game.cover.url.replace("thumb", "720p")}`}/>
+                                : <img alt="Placeholder Photo" loading="lazy" className="absolute left-0 top-0 w-full h-full object-cover object-center transition duration-50" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fno-image-available-sign-vector-id1138179183%3Fk%3D6%26m%3D1138179183%26s%3D612x612%26w%3D0%26h%3DprMYPP9mLRNpTp3XIykjeJJ8oCZRhb2iez6vKs8a8eE%3D&f=1&nofb=1&ipt=7727c264fde6fb0879a2b7b4ff990b45ce06dc0dd368db646c6be4f8356018b7&ipo=images" />
+                            }
+                        </Link>
+                        </div>
+                    </div>
+                    <div className="relative p-4">
+                        <p className="text-4xl text-white border-b border-black w-full">
+                        {
+                            game.name ? game.name
+                            : ""
+                        }
+                        </p>
+                        <p className="text-md text-white mt-1 line-clamp-2">
+                        {
+                            game.summary ? game.summary
+                            : ""
+                        }
+                        </p>
+                        
+                        <p className="absolute bottom-2 text-sm text-white mt-1 line-clamp-2">Favorites: {game.favoriteAmount}</p>                       
+                    </div>                       
+                </div>
+            </div>
+            
             </li>
-            </>
         })
     }
-    
-    </>
+    </ul>
+    </div>
+        : <div className={`bg-gray-300 pt-72px h-screen`}></div>
+    }
+    </div>
 }
